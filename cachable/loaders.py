8bf8800a6d4af9af73f35b65e1c3cba099712b3e
Klas Leino <kleino@live.com>
2020-06-11 15:30:58 -0400

    add numpy and keras loaders

--- cachable/loaders.py
+++ cachable/loaders.py
@@ -10,7 +10,7 @@ class Loader(object):
         raise NotImplementedError()
 
 
-class PickleLoader(object):
+class PickleLoader(Loader):
 
     def load(self, filename):
         with open(filename + '.pkl', 'rb') as f:
@@ -19,4 +19,42 @@ class PickleLoader(object):
     def save(self, filename, obj):
         with open(filename + '.pkl', 'wb') as f:
             pickle.dump(obj, f)
+
+
+try:
+    import numpy as np
+
+    class NumpyLoader(Loader):
+
+        def load(self, filename):
+            return np.load(filename + '.npy')
+
+        def save(self, filename, array):
+            np.save(filename + '.npy', array)
+
+except:
+    # Only include if numpy is installed.
+    pass
+
+
+try:
+    from keras.models import load_model
+
+    class KerasModelLoader(Loader):
+
+        def __init__(self, custom_objects=None):
+            self.custom_objects = (
+                {} if custom_objects is None else custom_objects)
+
+        def load(self, filename):
+            return load_model(
+                filename + '.h5', 
+                custom_objects=self.custom_objects)
+
+        def save(self, filename, model):
+            model.save(filename + '.h5')
+
+except:
+    # Only include if keras is installed.
+    pass
      
